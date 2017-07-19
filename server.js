ec5097a8ab29cee563c214593342748bc2c17217
Gijs <gijs@hackyourfuture.net>
2017-07-19 17:46:14 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const hbs = require('hbs');
+var app = express();
+const fs = require('fs');
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+app.use((req,res,next) =>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} : ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '/n' , (err) => {
+        if(err){
+        console.log('unable to append to server.log');
+        }
+    });
+    next();
+})
+
+app.use((req,res,next) =>{
+    res.render('maintenance.hbs');
+})
+
+
+hbs.registerHelper('getCurrentYear', () =>{
+        return new Date().getFullYear();
+});
+
+hbs.registerHelper('upperCaseIt', (words) => {
+    return words.toUpperCase();
+})
+//.get (first argument determines the page root, and enables you to create subpages sug as /about)
+//second argument is a function, it determines what to send back when getting a request from user. (req = info about request, res = response that determins what to send back to user )
+
+app.get('/', (req, res) =>{
+    res.render('home.hbs',{
+         pageTitle : 'Home page',
+        welcomeMessage : "HELLOW USER! WELCOME TO MY WEBZAAIT"
+    })
+});
+
+app.get('/about', (req,res) =>{
+    res.render('about.hbs', {
+        pageTitle : 'about page',
+        welcomeMessage : "HELLOW USER! WELCOME TO MY WEBZAAIT"
+    });
+});
+
+app.get('/error',(req,res) =>{
+    res.send({error:"DE PAGINA DOET HET NEIT JONGEN!",
+         errorMessage:"something is going wruung"});
+
+});
+
+//determine the port 
+app.listen((3000),() => {
+console.log('server is up on port 3000')});
